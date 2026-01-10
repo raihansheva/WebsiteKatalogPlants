7c3b22bfdd1103c874d7eb6447d2d4af03aef5ec
raihansheva <shevareva22@gmail.com>
2026-01-10 22:21:43 +0700

    ganti favicon website

--- pages/layout/header.php
+++ pages/layout/header.php
@@ -7,6 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/png" href="/WebsiteKatalogPlants/asset/image/favIcon.png">
     <title>Floratify</title>
     <?php if (!empty($page_css)): ?>
         <link rel="stylesheet" href="/WebsiteKatalogPlants/asset/style/main.css" />
